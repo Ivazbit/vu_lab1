b6edf011a5a8b8bcd950a49503d615c7f50896c9
Lukas A <lukas.aleksa@agmis.lt>
2022-02-20 12:48:37 +0200

    Added change text button

--- app/src/main/java/com/example/vu_lab1/MainActivity.kt
+++ app/src/main/java/com/example/vu_lab1/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.vu_lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val simpleButton = findViewById<Button>(R.id.simple_button)
+        val textView = findViewById<TextView>(R.id.text_view)
+
+        var simpleClicked = false
+        var colorClicked = false
+
+        simpleButton.setOnClickListener {
+            simpleClicked = if (!simpleClicked) {
+                textView.setText(R.string.hello_world_lt)
+                true
+            } else {
+                textView.setText(R.string.hello_world)
+                false
+            }
+        }
     }
 }
